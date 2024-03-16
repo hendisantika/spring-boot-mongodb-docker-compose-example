94ca8146891b965c904c53487102f7abdd171e72
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 05:46:18 +0700

    Add testInsertDeleteGet in BookRepositoryTest 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springbootmongodb/BookRepositoryTest.java
+++ src/test/java/id/my/hendisantika/springbootmongodb/BookRepositoryTest.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.springbootmongodb;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +27,22 @@ class BookRepositoryTest {
     @BeforeEach
     public void clean() {
         bookRepo.deleteAll();
+    }
+
+    @Test
+    void testInsertDeleteGet() {
+
+        bookRepo.insert(new Book(1, "Lord Of the Rings", "J.R.R. Tolkien"));
+
+        Book result = bookRepo.findById(1).get();
+
+        Assertions.assertEquals("Lord Of the Rings", result.name());
+
+        bookRepo.deleteById(1);
+
+        Optional<Book> result2 = bookRepo.findById(1);
+
+        Assertions.assertNull(result2.orElse(null));
+
     }
 }
